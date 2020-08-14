6e314fca6b1b0237dc8bb88bbbfa5ae11b831aef
C7ABT <connor.zhaozixuan@gmail.com>
2020-08-14 10:31:08 +0800

    added Problem 101

--- Solution.java
+++ Solution.java
@@ -484,8 +484,16 @@ public class Solution {
     }
     
     // 101. Symmetric Tree
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }   else if (t1 == null || t2 == null) {
+            return false;
+        }
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
     public boolean isSymmetric(TreeNode root) {
-        
+        return isMirror(root, root);
     }
 
 
