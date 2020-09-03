6f1f32afbbeaa6da8df807c0fc0c2eb56657c29f
C7ABT <connor.zhaozixuan@gmail.com>
2020-09-03 16:08:01 +0800

    added Problem 2

--- Java/2.add-two-numbers.java
+++ Java/2.add-two-numbers.java
@@ -17,7 +17,28 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
+        int carry = 0;
+        int sum = 0;
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (l1 != null || l2 != null) {
+            int x = (l1 == null) ? 0 : l1.val;
+            int y = (l2 == null) ? 0 : l2.val;
+            sum = x + y + carry;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }    
+        if (carry != 0) {
+            cur.next = new ListNode(carry);
+        }
+        return head.next;
     }
 }
 // @lc code=end
---
+++ Java/3.longest-substring-without-repeating-characters.java
@@ -0,0 +1,18 @@
+import sun.security.util.Length;
+
+/*
+ * @lc app=leetcode id=3 lang=java
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int count = 0, result = 0, pos = 0;
+        for (int i = 0; i < )
+
+    }
+}
+// @lc code=end
+
